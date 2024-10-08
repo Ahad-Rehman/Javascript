dd77a8cf6bcff6905101a31a7bb52c1f6f984616
Ahad-Rehman <157304468+Ahad-Rehman@users.noreply.github.com>
2024-10-08 16:34:50 +0000

    please fucking changes

---
+++ 1JS/06-strings.js
@@ -0,0 +1,15 @@
+const name = "ahad"
+const age = 18
+console.log(`my name is ${name} and my age is ${20}`);
+//concatination
+const newGame = new String ("iGikfikfgishgi")
+console.log(newGame.indexOf('G'));
+// let myName = "ahad"
+// const newString = myName.substring(0,2)
+// console.log(newString) 
+// substring print number from index you want
+// slice cut number from where you want to where at 
+// trim remove unusual spaces 
+// replace it replaces any value you want 
+// many more
+console.log(newGame.split("i,g"))
